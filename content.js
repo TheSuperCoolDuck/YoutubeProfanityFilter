ced9f235e45b11b44f1de48a30e8c2d0f6902ce1
TheSuperCoolDuck <Dchen0058@gmail.com>
2021-09-18 20:35:42 +1000

    Fixed muting edge cases

--- content.js
+++ content.js
@@ -5,6 +5,7 @@ let isCensoring = false;
 let blacklistTranscript = [];
 let currentTranscriptLine = null;
 let censorBlacklistLoop = null;
+let defaultMuted = false;
 
 chrome.runtime.onMessage.addListener((msg)=>{
     if(msg.from=="popup" && msg.subject=="checkCensoringState") {
@@ -60,30 +61,44 @@ function useProfanityFilter(){
 }
 
 function censorVideo(video){
+
+    chrome.runtime.sendMessage({from:"content",subject:"setBeepVolume",data:video.volume});
+
+    if(!isCensoring&&video.muted){
+        defaultMuted=true;
+    }
+    if(!video.muted){
+        defaultMuted=false;
+    }
+
+    console.log(defaultMuted);
+
     if(video!=null && !video.paused){
         if(!isCensoring){
             for(let i = 0; i < blacklistTranscript.length; i++) {
                 if(isWithinLine(blacklistTranscript[i]["startTime"], blacklistTranscript[i]["duration"], video.currentTime)) {
                    console.log(`the word "${blacklistTranscript[i]["text"]}" was filtered for ${blacklistTranscript[i]["duration"]} seconds`);
                    currentTranscriptLine = blacklistTranscript[i];
+                   if(!defaultMuted){
+                        chrome.runtime.sendMessage({from:"content",subject:"beepVideo"});
+                   }
                    video.muted = true;
-                   chrome.runtime.sendMessage({from:"content",subject:"beepVideo"});
                    isCensoring=true;
                    return;
                 }
             }
         } 
         else if(!isWithinLine(currentTranscriptLine["startTime"],currentTranscriptLine["duration"],video.currentTime)){
-             video.muted = false;
-             chrome.runtime.sendMessage({from:"content",subject:"unbeepVideo"});
-             isCensoring = false;
+            chrome.runtime.sendMessage({from:"content",subject:"unbeepVideo"});
+            isCensoring = false;
+            if(!defaultMuted){
+                video.muted = false;
+            }
         }
     }else if(isCensoring){
         chrome.runtime.sendMessage({from:"content",subject:"unbeepVideo"});
         isCensoring=false;
     }
-    
-    chrome.runtime.sendMessage({from:"content",subject:"setBeepVolume",data:video.volume});
 }
 
 function isWithinLine(startTime, duration, currentTime){
